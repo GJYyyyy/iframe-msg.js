44b05b801644f619163bddaa22e05a1a4bcb31b4
GJYyyyy <772048440@qq.com>
2023-07-27 15:17:37 +0800

    init

---
+++ src/IframeMsg.js
@@ -0,0 +1,41 @@
+class IframeMsg extends EventTarget {
+
+    data; // 接收到的信息
+
+    /**
+     * 通信对方的window对象
+     * @param {Window} _window 
+     */
+    constructor(_window) {
+        super();
+        this._window = _window;
+        this.init();
+    }
+
+    /**
+     * 初始化接收信息处理函数
+     */
+    init() {
+        const onMessage = (ev) => {
+            if (ev.data) {
+                const { action, data } = ev.data;
+                if (action === undefined) return;
+                this.data = ev.data;
+                this.dispatchEvent(new Event('message'));
+            }
+        }
+        window.addEventListener('message', onMessage, false);
+    }
+
+    /**
+     * 发送消息
+     * @param {String} action 动作
+     * @param {any} data 消息体
+     */
+    send(action, data) {
+        this._window.postMessage({
+            action,
+            data,
+        }, '*');
+    }
+}
